997d64ced37d8c8340d42f4f7963a9569b2219b7
chandan.m <chandan.m@pepperfry.com>
2023-08-13 16:10:45 +0530

    spelling

--- src/components/dropdown/Dropdown.js
+++ src/components/dropdown/Dropdown.js
@@ -1,7 +1,7 @@
 const Dropdown = () => {
     return ( 
         <div className="dropdown">
-            <h1>tetsing for github</h1>
+            <h1>testing for github</h1>
         </div>
      );
 }
